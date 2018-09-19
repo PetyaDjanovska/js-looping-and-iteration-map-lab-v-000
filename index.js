5a5fa67f19ae7d22a6544fb52cd8e10a4dcd5020
Petya Djanovska <petya.djanovska@gmail.com>
2018-09-19 15:41:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ function lowerCaseDrivers(drivers) {
   const driversLowerCase = drivers.map(dr => dr.toLowerCase());
   return driversLowerCase;
 }
+
+function nameToAttributes(drivers) {
+  const driversToObjects = drivers.map(function(names){
+    name => {firstName: names.slice(0, str.indexOf(' ')), lastName: names.slice(str.indexOf(' ')+1, str.length) }
+    const firstName = names.slice(0, str.indexOf(' '));
+    const lastName = names.slice(str.indexOf(' ')+1, str.length);
+
+  })
+}
