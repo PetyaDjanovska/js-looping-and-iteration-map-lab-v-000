7ae6684e0d2fafd478d9f722d0fdbe778156ef5a
Petya Djanovska <petya.djanovska@gmail.com>
2018-09-19 15:43:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function lowerCaseDrivers(drivers) {
 
 function nameToAttributes(drivers) {
   const driversToObjects = drivers.map(
-    names => {firstName: names.slice(0, str.indexOf(' ')), lastName: names.slice(str.indexOf(' ')+1, str.length)}
+    names => names({firstName: names.slice(0, str.indexOf(' ')), lastName: names.slice(str.indexOf(' ')+1, str.length)});
   )
 }
